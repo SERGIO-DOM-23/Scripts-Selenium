91aac75fddb4fbbb49efc07fb4964bced33d4273
SERGIO-DOM-23 <sergiodominguez21sdd@gmail.com>
2024-03-23 22:22:38 -0300

    modificamos totalmente el scrip, incluimos el realizado por el profe.

--- src/test/java/loginTest.java
+++ src/test/java/loginTest.java
@@ -1,27 +1,42 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class loginTest {
-
     private WebDriver driver;
 
-    //indica por medio de jUnit la configuracion para el test (loginTest)
+    //indica por medio de junit la configuracion para este test (clase loginTest)
     @Before
-    public void setUp(){}
+    public void setUp(){
+        //indica donde esta el driver con el que voy a automatizar
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+        //le indicamos a la variable driver de selenium, que vamos a trabajar con el driver de chrome
+        driver = new ChromeDriver();
+        //maximizar la pantalla
+        driver.manage().window().maximize();
+        //indicar al driver que se dirija a la pagina que vamos a testear
+        driver.get("http://demo.guru99.com/test/newtours/");
+    }
 
-    System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
-    driver = new ChromeDriver();
-
-    // aqui se encunetra "el cuerpo de la automatizacion"
+    //el cuerpo de la automatizacion
     @Test
-    public void testLogin(){}
+    public void testLogin(){
+        WebElement username = driver.findElement(By.xpath("//input[@name='userName']"));
+        WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
+        WebElement botonSubmit = driver.findElement(By.xpath("//input[@name='submit']"));
 
-    // indica lo realizado, luego de terminado el test
+        username.sendKeys("sergio");
+        password.sendKeys("123456");
+        botonSubmit.click();
+    }
+
+    //indica lo que se realiza luego de terminar el test
     @After
-    public void tearDwn(){}
-
+    public void tearDown(){
+        driver.quit();
+    }
 }
